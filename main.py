42c88d9bf751de7293029c731d8bb9e95357dde0
whatwouldtuxdo <lindsey@novanet.org>
2020-03-09 22:44:53 -0400

    Added timestamps

--- main.py
+++ main.py
@@ -9,17 +9,22 @@ port = os.environ.get('PORT', 80)
 while True:
     error_count = 0 
 
-    print("# HELP dns_validation Status of Querying DNS endpoint")
-    print("# TYPE dns_validation summary")
+    print("# HELP dns_validation_lookup Status of Querying DNS endpoint")
+    print("# TYPE dns_validation_lookup summary")
+    print("# TYPE dns_validation_response_type summary")
     try:
         start_time = time.time()
         result = socket.getaddrinfo(domain, port)
         end_time = time.time()
         total_time = (end_time - start_time)
-        print("dns_validation_lookup {}".format(result[2][4][0]))
-        print("dns_validation_response_time {}".format(total_time))
+        print("dns_validation_lookup {} {}".format(result[2][4][0], time.time()))
+        print("dns_validation_response_time {} {}".format(total_time, time.time()))
     except socket.gaierror:
-        error_count=error_count+1
+        '''
+        If we receive a GAIERRROR, name resolution has failed.
+        Increment our counter by one and display counter total.
+        '''
+        error_count += 1
         print("# TYPE dns_validation_error_count counter")
-        print("dns_validation_error_count {}".format(error_count))
+        print("dns_validation_error_count {} {}".format(error_count), time.time())
     time.sleep(5)
