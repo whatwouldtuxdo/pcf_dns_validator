b3b5ad1fec8cbb72d681840be75f6c39a46e3bce
whatwouldtuxdo <lindsey@novanet.org>
2020-03-09 22:38:29 -0400

    Replicating prometheus outputs, need to update to be valid

--- main.py
+++ main.py
@@ -7,12 +7,18 @@ port = os.environ.get('PORT', 80)
 
 
 while True:
+    error_count = 0 
+
+    print("# HELP dns_validation Status of Querying DNS endpoint")
+    print("# TYPE dns_validation summary")
     try:
         start_time = time.time()
         result = socket.getaddrinfo(domain, port)
         end_time = time.time()
         total_time = (end_time - start_time) * 10000
-        print("Success - %s - Completed in %.2fms." % (result[2][4][0], total_time))
+        print("dns_validation_lookup {}".format(result[2][4][0]))
+        print("dns_validation_response_time {}".format(total_time))
     except socket.gaierror:
-        print("ERROR - DNS Validation")
+        error_count=error_count+1
+        print("dns_validation_error_count {}".format(error_count))
     time.sleep(5)
